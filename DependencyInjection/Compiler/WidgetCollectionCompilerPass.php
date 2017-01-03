b2c3342e42760cdf52c5d677b772b8d2d2206fdc
Evgeniy.it <evgeniy.it@gmail.com>
2017-01-03 21:40:11 +0200

    doctrine fix

--- DependencyInjection/Compiler/WidgetCollectionCompilerPass.php
+++ DependencyInjection/Compiler/WidgetCollectionCompilerPass.php
@@ -2,8 +2,10 @@
 
 namespace Evgit\Bundle\WidgetBundle\DependencyInjection\Compiler;
 
+use Evgit\Bundle\WidgetBundle\Widget\RepositoryWidget;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Reference;
 
 /**
@@ -18,11 +20,34 @@ class WidgetCollectionCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        if (!$container->has('Evgit.widget.widget_collection')) {
+        if ($container->has('doctrine')) {
+            $this->addRepositoryService($container);
+        }
+
+        $this->addTaggedServices($container);
+    }
+
+    private function addRepositoryService(ContainerBuilder $container)
+    {
+        $definition = new DefinitionDecorator('evgit.widget.abstract_widget');
+        $definition->setClass(RepositoryWidget::class);
+
+        $definition->addMethodCall('setName', ['repository']);
+        $definition->addMethodCall('setDoctrine', [new Reference('doctrine')]);
+        $definition->addTag("widget");
+
+        $container->setDefinition("evgit.widget.repository_widget", $definition);
+
+        return;
+    }
+
+    private function addTaggedServices(ContainerBuilder $container)
+    {
+        if (!$container->has('evgit.widget.widget_collection')) {
             return;
         }
         $widgets = $container->findTaggedServiceIds("widget");
-        $widgetCollectionRenderer = $container->findDefinition("Evgit.widget.widget_collection");
+        $widgetCollectionRenderer = $container->findDefinition("evgit.widget.widget_collection");
 
         foreach ($widgets as $widget => $tags) {
             $widgetCollectionRenderer->addMethodCall("addWidget", [new Reference($widget)]);
